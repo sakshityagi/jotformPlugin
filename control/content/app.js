f8990eec667d2ef84f906998e8bc8023217c7114
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-01-08 11:53:22 +0530

    Added utils service to validate url

--- control/content/app.js
+++ control/content/app.js
@@ -2,7 +2,11 @@
 
 (function (angular) {
   angular.module('jotFormPluginContent', ['ui.bootstrap'])
-    .controller('ContentHomeCtrl', function(){
-
-    });
+    .controller('ContentHomeCtrl', ['Utils', function (Utils) {
+      var ContentHome = this;
+      ContentHome.validateUrl = function () {
+        var result = Utils.validateUrl(ContentHome.content.url);
+        console.log("?????????????????", result);
+      };
+    }]);
 })(window.angular);
--- control/content/app.services.js
+++ control/content/app.services.js
@@ -1,66 +1,89 @@
 'use strict';
 
 (function (angular, buildfire) {
-    angular.module('jotFormPluginContent')
-        .provider('Buildfire', [function () {
-            var Buildfire = this;
-            Buildfire.$get = function () {
-                return buildfire
-            };
-            return Buildfire;
-        }])
-        .factory("DataStore", ['Buildfire', '$q', 'STATUS_CODE', 'STATUS_MESSAGES', function (Buildfire, $q, STATUS_CODE, STATUS_MESSAGES) {
-            return {
-                get: function (_tagName) {
-                    var deferred = $q.defer();
-                    Buildfire.datastore.get(_tagName, function (err, result) {
-                        if (err) {
-                            return deferred.reject(err);
-                        } else if (result) {
-                            return deferred.resolve(result);
-                        }
-                    });
-                    return deferred.promise;
-                }, update: function (_id, _item, _tagName) {
-                    var deferred = $q.defer();
-                    if (typeof _id == 'undefined') {
-                        return deferred.reject(new Error({
-                            code: STATUS_CODE.UNDEFINED_ID,
-                            message: STATUS_MESSAGES.UNDEFINED_ID
-                        }));
-                    }
-                    if (typeof _item == 'undefined') {
-                        return deferred.reject(new Error({
-                            code: STATUS_CODE.UNDEFINED_DATA,
-                            message: STATUS_MESSAGES.UNDEFINED_DATA
-                        }));
-                    }
-                    Buildfire.datastore.update(_id, _item, _tagName, function (err, result) {
-                        if (err) {
-                            return deferred.reject(err);
-                        } else if (result) {
-                            return deferred.resolve(result);
-                        }
-                    });
-                    return deferred.promise;
-                },
-                save: function (_item, _tagName) {
-                    var deferred = $q.defer();
-                    if (typeof _item == 'undefined') {
-                        return deferred.reject(new Error({
-                            code: STATUS_CODE.UNDEFINED_DATA,
-                            message: STATUS_MESSAGES.UNDEFINED_DATA
-                        }));
-                    }
-                    Buildfire.datastore.save(_item, _tagName, function (err, result) {
-                        if (err) {
-                            return deferred.reject(err);
-                        } else if (result) {
-                            return deferred.resolve(result);
-                        }
-                    });
-                    return deferred.promise;
-                }
+  angular.module('jotFormPluginContent')
+    .provider('Buildfire', [function () {
+      var Buildfire = this;
+      Buildfire.$get = function () {
+        return buildfire
+      };
+      return Buildfire;
+    }])
+    .factory("DataStore", ['Buildfire', '$q', 'STATUS_CODE', 'STATUS_MESSAGES', function (Buildfire, $q, STATUS_CODE, STATUS_MESSAGES) {
+      return {
+        get: function (_tagName) {
+          var deferred = $q.defer();
+          Buildfire.datastore.get(_tagName, function (err, result) {
+            if (err) {
+              return deferred.reject(err);
+            } else if (result) {
+              return deferred.resolve(result);
             }
-            }])
+          });
+          return deferred.promise;
+        }, update: function (_id, _item, _tagName) {
+          var deferred = $q.defer();
+          if (typeof _id == 'undefined') {
+            return deferred.reject(new Error({
+              code: STATUS_CODE.UNDEFINED_ID,
+              message: STATUS_MESSAGES.UNDEFINED_ID
+            }));
+          }
+          if (typeof _item == 'undefined') {
+            return deferred.reject(new Error({
+              code: STATUS_CODE.UNDEFINED_DATA,
+              message: STATUS_MESSAGES.UNDEFINED_DATA
+            }));
+          }
+          Buildfire.datastore.update(_id, _item, _tagName, function (err, result) {
+            if (err) {
+              return deferred.reject(err);
+            } else if (result) {
+              return deferred.resolve(result);
+            }
+          });
+          return deferred.promise;
+        },
+        save: function (_item, _tagName) {
+          var deferred = $q.defer();
+          if (typeof _item == 'undefined') {
+            return deferred.reject(new Error({
+              code: STATUS_CODE.UNDEFINED_DATA,
+              message: STATUS_MESSAGES.UNDEFINED_DATA
+            }));
+          }
+          Buildfire.datastore.save(_item, _tagName, function (err, result) {
+            if (err) {
+              return deferred.reject(err);
+            } else if (result) {
+              return deferred.resolve(result);
+            }
+          });
+          return deferred.promise;
+        }
+      }
+    }])
+    .factory("Utils", ["$http", function ($http) {
+      return {
+        validateUrl: function (url) {
+          var regex = /https?:\/\/([a-z0-9\.]+)\.jotform\.(com|me)\/?(.*)/i;
+          var result = regex.test(url);
+          if (result) {
+            $http.get(url)
+              .success(function (response) {
+                console.log("**************", response);
+                if (response)
+                  return true;
+                else
+                  return null;
+              })
+              .error(function (error) {
+                return null;
+              });
+          }
+          else
+            return null;
+        }
+      }
+    }]);
 })(window.angular, window.buildfire);
