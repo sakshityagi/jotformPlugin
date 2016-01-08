2787d2e230fec305feba136b812424f26f6a824c
tabish.khan <tabish.khan@tothenew.com>
2016-01-08 15:55:10 +0530

    Added test cases for content section.

---
+++ test/control/content/app.spec.js
@@ -0,0 +1,75 @@
+describe('Unit: jotFormPluginContent content app', function () {
+  describe('Unit: app routes', function () {
+    beforeEach(module('jotFormPluginContent'));
+    var location, route, rootScope;
+    beforeEach(inject(function () {
+
+    }));
+    var ContentHome, scope, $rootScope, $controller, Buildfire, ActionItems, TAG_NAMES, STATUS_CODE, LAYOUTS, STATUS_MESSAGES, CONTENT_TYPE, q, Utils;
+
+    beforeEach(inject(function (_Utils_, _$rootScope_, _$q_, _$controller_, _TAG_NAMES_, _STATUS_CODE_, _STATUS_MESSAGES_) {
+      $rootScope = _$rootScope_;
+      q = _$q_;
+      scope = $rootScope.$new();
+      $controller = _$controller_;
+      TAG_NAMES = _TAG_NAMES_;
+      STATUS_CODE = _STATUS_CODE_;
+      STATUS_MESSAGES = _STATUS_MESSAGES_;
+      Utils = _Utils_;
+      Buildfire = {
+        components: {
+          carousel: {
+            editor: function (name) {
+              return {}
+            },
+            viewer: function (name) {
+              return {}
+            }
+          }
+
+        },     spinner: {
+          hide: function () {
+            return {}
+          },
+          show: function () {
+            return {}
+          }
+
+        }
+      };
+
+      ActionItems = jasmine.createSpyObj('ActionItems', ['showDialog']);
+    }));
+
+    beforeEach(function () {
+      ContentHome = $controller('ContentHomeCtrl', {
+        $scope: scope,
+        $q: q,
+        Buildfire: Buildfire,
+        TAG_NAMES: TAG_NAMES,
+        ActionItems: ActionItems,
+        STATUS_CODE: STATUS_CODE,
+        CONTENT_TYPE: CONTENT_TYPE,
+        LAYOUTS: LAYOUTS,
+        Utils:Utils
+      });
+    });
+    describe('It will test the defined methods', function () {
+      it('it should pass if ContentHome is defined', function () {
+        expect(ContentHome).not.toBeUndefined();
+      });
+      it('it should pass if clearData is called', function () {
+        ContentHome.clearData();
+      });
+
+      it('it should pass if ContentHome.validateUrl is called', function () {
+        ContentHome.validateUrl();
+      });
+
+
+    });
+    it("aaaaaa", function(){
+
+    })
+  });
+});
