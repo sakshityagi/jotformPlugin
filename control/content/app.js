a21623fa825d03e5ff7a0f0e120ad2a0c49ee6e6
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-01-08 09:48:23 +0530

    Added angular structure for content and widget section. Also added resources folder

---
+++ control/content/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function (angular) {
+  angular.module('jotFormPluginContent', ['ui.bootstrap'])
+    .controller('ContentHomeCtrl', function(){
+
+    });
+})(window.angular);
---
+++ widget/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function (angular) {
+  angular.module('jotFormPluginWidget', ['ui.bootstrap'])
+    .controller('WidgetHomeCtrl', function () {
+
+    });
+})(window.angular);
